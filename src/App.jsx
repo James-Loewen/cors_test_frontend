12057f56a71aaba7ffe2106aa0e86be00cac7ee8
James Loewen <jamesfloewen@gmail.com>
2023-07-11 08:52:59 -0400

    display current user on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { getCookie } from "./utils/cookieUtils";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   const handleLogIn = async (e) => {
     window.location.href = "https://pynoodler.pythonanywhere.com/accounts/login/";
     // window.location.href = "http://localhost:8000/accounts/login/"
@@ -13,10 +16,12 @@ function App() {
     });
     const data = await res.json();
     console.log(data);
+
+    setUser(data.user);
   }
 
   const handleLogCookie = async (e) => {
-    const csrfToken = getCookie('csrftoken');
+    const csrfToken = getCookie('sessionid');
     console.log(csrfToken);
   }
 
@@ -26,6 +31,12 @@ function App() {
       <div className="button-card">
         <button onClick={handleLogIn}>Log In</button>
         <button onClick={handleLogUser}>Log current user</button>
+        {user && (
+          <>
+            <p>Current user: <span style={{color: 'coral'}}>{user}</span></p>
+            <button onClick={() => setUser(null)}>clear</button>
+          </>
+        )}
         <button onClick={handleLogCookie}>Log CSRF token</button>
       </div>
     </>
